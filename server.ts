c28225e47088309a14c14e2b8f0e1b689ae08993
MarcoHernandezz <marco.hernandezsan@alumno.buap.mx>
2024-03-19 12:59:13 -0600

    Se modifico el envio de datos del cliente al servidor. En server.ts se agrego una funcion para hacer salas.

--- server.ts
+++ server.ts
@@ -1,165 +1,219 @@
+
 import { createServer } from "node:http";
 import { join } from "node:path";
 import { getIps } from "@kristall/get-ips";
 import express from "express";
-import type { Request, Response } from "express";
 import { Server } from "socket.io";
 
+import type { Request, Response } from "express";
 import type { ClientResponse, Task, User } from "./src/types";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-	maxHttpBufferSize: 1e16,
+    maxHttpBufferSize: 1e16,
 });
 
 app.use(express.static(join(__dirname, "client")));
 
 app.get("/", (req: Request, res: Response) => {
-	res.sendFile(join(__dirname, "dist/client", "index.html"));
+    res.sendFile(join(__dirname, "dist/client", "index.html"));
 });
 
 const users: User[] = [];
 
 const tasks: Task[] = [
-	{
-		id: 1,
-		name: "create_array",
-		response: "create_array",
-		prev: null,
-	},
-	{
-		id: 2,
-		name: "set_planets_positions",
-		response: "set_planets_positions",
-		prev: 1,
-	},
-	{
-		id: 3,
-		name: "filter_planets",
-		response: "filter_planets",
-		prev: 2,
-	},
-	{
-		id: 4,
-		name: "animation",
-		response: "animation",
-		prev: 3,
-	},
+    {
+        id: 1,
+        name: "create_array",
+        response: "create_array",
+        prev: null,
+    },
+    {
+        id: 2,
+        name: "set_planets_positions",
+        response: "set_planets_positions",
+        prev: 1,
+    },
+    {
+        id: 3,
+        name: "filter_planets",
+        response: "filter_planets",
+        prev: 2,
+    },
+    {
+        id: 4,
+        name: "animation",
+        response: "animation",
+        prev: 3,
+    },
 ];
 
 const keys = [
-	"create_array",
-	"set_planets_positions",
-	"filter_planets",
-	"animation",
+    "create_array",
+    "set_planets_positions",
+    "filter_planets",
+    "animation",
 ].reverse();
 
+function getRandomTask(): Task | null {
+    if (tasks.length === 0) return null;
+    const randomIndex = Math.floor(Math.random() * tasks.length);
+    return tasks[randomIndex];
+}
+
 server.listen(3000, getIps()[0].address, () => {
-	console.log(`listening on http://${getIps()[0].address}:3000`);
+    console.log(`listening on http://${getIps()[0].address}:3000`);
 });
 
 const getUserKey = (userId: string): string | null =>
-	users.find((user) => user.id === userId)?.key ?? null;
+    users.find((user) => user.id === userId)?.key ?? null;
 
 const responses: ClientResponse[] = [];
 
 io.on("connection", (socket) => {
-	socket.on("userId", (userId) => {
-		const isUserOnList = users.some((user) => user.id === userId);
-		if (isUserOnList) {
-			socket.emit(`user-${userId}`, getUserKey(userId));
-			return;
+    socket.on("userId", (userId) => {
+        const isUserOnList = users.some((user) => user.id === userId);
+        if (isUserOnList) {
+            socket.emit(`user-${userId}`, getUserKey(userId));
+            return;
+        }
+
+        if (keys.length === 0) {
+            socket.emit(`user-${userId}`, {
+                message: "No more keys available",
+                info: responses[responses.length - 1].response,
+            });
+            return;
+        }
+        users.push({ id: userId, socketId: socket.id, key: keys.pop() as string });
+        socket.emit(`user-${userId}`, getUserKey(userId));
+    });
+
+    socket.on("create_array", (data) => {
+        console.log("createArray");
+        const hasAlreadyResponse = responses.some(
+            (response) => response.id === data.id,
+        );
+        if (hasAlreadyResponse) {
+            const planetPositionUser = users.find(
+                (user) => user.key === "set_planets_positions",
+            );
+            if (!planetPositionUser) return;
+            socket.emit(`user-${planetPositionUser.id}-data`, {
+                key: "set_planets_positions",
+                info: data,
+            });
+            return;
+        }
+        responses.push({
+            id: data.id,
+            response: data.response,
+            key: "create_array",
+        });
+        const planetPositionUser = users.find(
+            (user) => user.key === "set_planets_positions",
+        );
+        if (!planetPositionUser) return;
+        socket.emit(`user-${planetPositionUser.id}-data`, {
+            key: "set_planets_positions",
+            info: data,
+        });
+    });
+
+    const rooms = new Map<string, string>(); // Mapa para almacenar las salas y su tarea asignada
+
+    socket.on("userId", (userId) => {
+        const isUserOnList = users.some((user) => user.id === userId);
+        if (isUserOnList) {
+            socket.emit(`user-${userId}`, getUserKey(userId));
+            return;
+        }
+
+        if (keys.length === 0) {
+            socket.emit(`user-${userId}`, {
+                message: "No more keys available",
+                info: responses[responses.length - 1].response,
+            });
+            return;
+        }
+        users.push({ id: userId, socketId: socket.id, key: keys.pop() as string });
+        socket.emit(`user-${userId}`, getUserKey(userId));
+    });
+
+    socket.on("create_array", (data) => {
+        console.log("createArray");
+        const hasAlreadyResponse = responses.some(
+            (response) => response.id === data.id,
+        );
+        if (hasAlreadyResponse) {
+            const planetPositionUser = users.find(
+                (user) => user.key === "set_planets_positions",
+            );
+            if (!planetPositionUser) return;
+            socket.emit(`user-${planetPositionUser.id}-data`, {
+                key: "set_planets_positions",
+                info: data,
+            });
+            return;
+        }
+        responses.push({
+            id: data.id,
+            response: data.response,
+            key: "create_array",
+        });
+        const planetPositionUser = users.find(
+            (user) => user.key === "set_planets_positions",
+        );
+        if (!planetPositionUser) return;
+        socket.emit(`user-${planetPositionUser.id}-data`, {
+            key: "set_planets_positions",
+            info: data,
+        });
+    });
+
+    socket.on("set_planets_positions", (data) => {
+        console.log("setPlanetsPositions");
+        responses.push({
+            id: data.id,
+            response: data.response,
+            key: "set_planets_positions",
+        });
+        console.log(responses[1]);
+    });
+
+    socket.on("filter_planets", (data) => {
+        const hasAlreadyResponse = responses.some(
+            (response) => response.id === data.id,
+        );
+        if (hasAlreadyResponse) return;
+        responses.push({
+            id: data.id,
+            response: data.response,
+            key: "filter_planets",
+        });
+        socket.emit("animation", {
+            info: data.response,
+        });
+    });
+
+    socket.on("get-animation", (data) => {
+        socket.emit("animation", {
+            info: responses[2].response,
+        });
+    });
+
+	socket.on('join-room', (roomName) => {
+		socket.join(roomName);
+		const userId = socket.id;
+		if (!rooms.has(roomName)) {
+			const randomTask = getRandomTask(); // Función para obtener una tarea aleatoria
+			if (randomTask) {
+				rooms.set(roomName, randomTask.name); // Aquí se asume que 'name' es una propiedad de 'Task' que contiene el nombre de la tarea
+			}
 		}
-
-		if (keys.length === 0) {
-			socket.emit(`user-${userId}`, {
-				message: "No more keys available",
-				info: responses[responses.length - 1].response,
-			});
-			return;
-		}
-		users.push({ id: userId, socketId: socket.id, key: keys.pop() as string });
-		socket.emit(`user-${userId}`, getUserKey(userId));
+		const task = rooms.get(roomName);
+		socket.emit('assigned-task', task);
 	});
-
-	socket.on("create_array", (data) => {
-		console.log("createArray");
-		const hasAlreadyResponse = responses.some(
-			(response) => response.id === data.id,
-		);
-		if (hasAlreadyResponse) {
-			const planetPositionUser = users.find(
-				(user) => user.key === "set_planets_positions",
-			);
-			if (!planetPositionUser) return;
-			socket.emit(`user-${planetPositionUser.id}-data`, {
-				key: "set_planets_positions",
-				info: data,
-			});
-			return;
-		}
-		responses.push({
-			id: data.id,
-			response: data.response,
-			key: "create_array",
-		});
-		const planetPositionUser = users.find(
-			(user) => user.key === "set_planets_positions",
-		);
-		if (!planetPositionUser) return;
-		socket.emit(`user-${planetPositionUser.id}-data`, {
-			key: "set_planets_positions",
-			info: data,
-		});
-	});
-
-	socket.on("get-data", ({ key, id }) => {
-		const prev = tasks.find((task) => task.name === key);
-
-		if (!prev) return;
-
-		const isPrevNumber = typeof prev.prev === "number";
-		const previousKey = isPrevNumber
-			? tasks[(prev.prev as number) - 1].name
-			: null;
-
-		const data = responses.find((response) => response.key === previousKey);
-
-		socket.emit(`user-${id}-data`, {
-			key,
-			info: data ? data.response : null,
-		});
-	});
-
-	socket.on("set_planets_positions", (data) => {
-		console.log("setPlanetsPositions");
-		responses.push({
-			id: data.id,
-			response: data.response,
-			key: "set_planets_positions",
-		});
-		console.log(responses[1]);
-	});
-
-	socket.on("filter_planets", (data) => {
-		const hasAlreadyResponse = responses.some(
-			(response) => response.id === data.id,
-		);
-		if (hasAlreadyResponse) return;
-		responses.push({
-			id: data.id,
-			response: data.response,
-			key: "filter_planets",
-		});
-		socket.emit("animation", {
-			info: data.response,
-		});
-	});
-
-	socket.on("get-animation", (data) => {
-		socket.emit("animation", {
-			info: responses[2].response,
-		});
-	});
+	
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import io from "socket.io-client";
-import { v4 as uuidv4 } from "uuid";
+import { io } from "socket.io-client";
+import { v4 as uuidv4 } from "uuid"; // Agregamos la importación de uuidv4
 import type {
-	CreateArrayResponse,
-	PlanetsPositions,
-	ServerResponse,
+    CreateArrayResponse,
+    PlanetsPositions,
+    ServerResponse,
 } from "./types";
 import animatePlanets from "./utils/animation";
 import createArrays from "./utils/createArrays";
@@ -11,79 +11,77 @@ import filterArrays from "./utils/filterArrays";
 import setPlanetsPositions from "./utils/setPlanetsPositions";
 
 const hasId = window.localStorage.getItem("id") ?? null;
-
 const id = hasId ? hasId : uuidv4();
+const socket = io(); // Utiliza la misma instancia de socket del servidor del segundo código
 
-const socket = io();
 document.addEventListener("DOMContentLoaded", () => {
-	if (!hasId) {
-		window.localStorage.setItem("id", id);
-	}
-	socket.emit("userId", id);
+    if (!hasId) {
+        window.localStorage.setItem("id", id);
+    }
+    socket.emit("userId", id);
 });
 
 socket.on(`user-${id}`, (data) => {
-	window.localStorage.setItem("key", data);
+    window.localStorage.setItem("key", data);
 
-	if (data.message === "No more keys available") {
-		const { planet1PositionFiltered, planet2PositionFiltered } = data.info;
-		animatePlanets(planet1PositionFiltered, planet2PositionFiltered);
-		return;
-	}
+    if (data.message === "No more keys available") {
+        const { planet1PositionFiltered, planet2PositionFiltered } = data.info;
+        animatePlanets(planet1PositionFiltered, planet2PositionFiltered);
+        return;
+    }
 
-	const key = data;
+    const key = data;
 
-	socket.emit("get-data", {
-		key,
-		id,
-	});
+    socket.emit("get-data", {
+        key,
+        id,
+    });
 });
 
 socket.on(
-	`user-${id}-data`,
-	(data: ServerResponse<CreateArrayResponse | PlanetsPositions>) => {
-		if (data.key === "create_array") {
-			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-			const response = createArrays() as any;
-			socket.emit("create_array", {
-				response,
-				id,
-			});
-		} else if (data.key === "set_planets_positions") {
-			const positions = setPlanetsPositions([
-				(data.info as CreateArrayResponse)[0],
-				(data.info as CreateArrayResponse)[1],
-			]);
+    `user-${id}-data`,
+    (data: ServerResponse<CreateArrayResponse | PlanetsPositions>) => {
+        if (data.key === "create_array") {
+            const response = createArrays() as any;
+            socket.emit("create_array", {
+                response,
+                id,
+            });
+        } else if (data.key === "set_planets_positions") {
+            const positions = setPlanetsPositions([
+                (data.info as CreateArrayResponse)[0],
+                (data.info as CreateArrayResponse)[1],
+            ]);
 
-			socket.emit("set_planets_positions", {
-				response: positions,
-				id,
-			});
-		} else if (data.key === "filter_planets") {
-			const response = filterArrays({
-				planetPositions1: (data.info as PlanetsPositions).planetPositions1,
-				planetPositions2: (data.info as PlanetsPositions).planetPositions2,
-			});
-			socket.emit("filter_planets", {
-				response,
-				id,
-			});
-		} else if (data.key === "animation") {
-			const { planetPositions1, planetPositions2 } =
-				data.info as PlanetsPositions;
+            socket.emit("set_planets_positions", {
+                response: positions,
+                id,
+            });
+        } else if (data.key === "filter_planets") {
+            const response = filterArrays({
+                planetPositions1: (data.info as PlanetsPositions).planetPositions1,
+                planetPositions2: (data.info as PlanetsPositions).planetPositions2,
+            });
+            socket.emit("filter_planets", {
+                response,
+                id,
+            });
+        } else if (data.key === "animation") {
+            const { planetPositions1, planetPositions2 } =
+                data.info as PlanetsPositions;
 
-			animatePlanets(planetPositions1, planetPositions2);
-		}
-	},
+            animatePlanets(planetPositions1, planetPositions2);
+        }
+    },
 );
 
 socket.on("animation", (data) => {
-	const { planetPositions1, planetPositions2 } = data.info as PlanetsPositions;
+    const { planetPositions1, planetPositions2 } = data.info as PlanetsPositions;
 
-	animatePlanets(planetPositions1, planetPositions2);
+    animatePlanets(planetPositions1, planetPositions2);
 });
 
 const button = document.querySelector("button") as HTMLButtonElement;
 button.addEventListener("click", () => {
-	socket.emit("get-animation");
+    socket.emit("get-animation");
 });
